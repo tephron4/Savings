9ab046036c0ff40285ad660c26474086a48df2ee
tephron4 <tobiasephron@gmail.com>
2020-07-30 23:15:54 -0400

    Created (or started) the SaveMain.java file

---
+++ SaveMain.java
@@ -0,0 +1,28 @@
+/**
+ * A program that will keep track of and organize my savings using a data base.
+ *
+ *
+ * @author Tobias Ephron
+ * @version Version 1 (Started: )
+ */
+
+import java.util.*;
+
+public class SaveMain{
+
+	/** Constructor Method */
+	public SaveMain(){
+
+	}
+
+	/**
+	 * main method
+	 *
+	 * @param args an array of Strings
+	 */
+	public static void main(String[] args){
+		System.out.println("This is the SaveMain program...");
+	}
+
+
+}
